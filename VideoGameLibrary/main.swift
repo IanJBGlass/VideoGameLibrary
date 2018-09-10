059ff068edff23c66bf3bedf21bfb558047cafef
Jacob Glass <jacobglass@MacBook-Pro-5.local>
2018-09-10 12:42:59 -0500

    Blank Project

---
+++ VideoGameLibrary/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VideoGameLibrary
+//
+//  Created by Jacob Glass on 9/10/18.
+//  Copyright © 2018 Jacob Glass. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
